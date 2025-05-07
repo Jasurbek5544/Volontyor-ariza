ad4eb3bb683cd30613cbaecb981fd0cf2e8953b7
snowbiro <snowbiro@gmail.com>
2025-05-07 20:45:11 +0500

    done3

--- ariza_tizimi/settings.py
+++ ariza_tizimi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,7 +115,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Statik fayllar uchun asosiy URL
+STATIC_URL = '/static/'
+
+# Statik fayllar yig‘iladigan joy (production uchun)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Agar loyihada o‘z shaxsiy statik fayllaringiz bo‘lsa:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
